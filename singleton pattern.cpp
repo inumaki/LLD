282fc8ddcb3fdca06dd28a49fdb339f68201b34d
aryan katiyar <69989774+inumaki@users.noreply.github.com>
2023-07-30 11:28:15 +0530

    Add files via upload

--- singleton pattern.cpp
+++ singleton pattern.cpp
@@ -1,42 +1,49 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <string>
 
 class ConfigurationManager {
-	
-	private:
-	static ConfigurationManager instance;
-	string currentTheme;
+private:
+    static ConfigurationManager* instance;
+    std::string currentTheme;
 
-	public:
-	ConfigurationManager() {
-	this->currentTheme = "light";	
-	}	
-	
-	ConfigurationManager(ConfigurationManager &obj) = delete; // to ensure that shallow copy is not created.
-	
-	static ConfigurationManager& Get() {
-		return instance;
-	}
-	
-	void getTheme() {
-		cout<<this->currentTheme<<endl;
-	}
-	void updateTheme() {
-		if(this->currentTheme == "dark")
-			this->currentTheme = 'light';
-		else
-			this->currentTheme = "dark";
-	}		
+    ConfigurationManager() {
+        this->currentTheme = "light";
+    }
+
+public:
+    ConfigurationManager(const ConfigurationManager&) = delete; // To prevent shallow copy
+
+    static ConfigurationManager* Get() {
+        if (!instance) {
+            instance = new ConfigurationManager();
+        }
+        return instance;
+    }
+
+    void getTheme() {
+        std::cout << this->currentTheme << std::endl;
+    }
+
+    void updateTheme() {
+        if (this->currentTheme == "dark")
+            this->currentTheme = "light";
+        else
+            this->currentTheme = "dark";
+    }
 };
 
-ConfigurationManager ConfigurationManager::instance; // not need to deal with null ,since references can't be null
+// Initialize the static member instance to nullptr
+ConfigurationManager* ConfigurationManager::instance = nullptr;
 
 int main() {
-	
-	ConfigurationManager &obj = ConfigurationManager::Get();
-	obj.getTheme();
-	obj.updateTheme();
-	ConfigurationManager &obj1 = ConfigurationManager::Get();
-	obj1.getTheme();
+    ConfigurationManager* obj = ConfigurationManager::Get();
+    obj->getTheme();
+    obj->updateTheme();
+    ConfigurationManager* obj1 = ConfigurationManager::Get();
+    obj1->getTheme();
 
+    // Clean up memory (optional, as it will be released when the program ends)
+    delete obj;
+
+    return 0;
 }
